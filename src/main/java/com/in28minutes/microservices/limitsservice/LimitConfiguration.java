a2a0a8124261f04e379e1ee04b1cd1ba48929b97
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-03-20 21:16:18 -0300

    Criado um simples rest controller

---
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitConfiguration.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.limitsservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LimitConfiguration {
+	private Integer maximum;
+	private Integer mininum;
+}
